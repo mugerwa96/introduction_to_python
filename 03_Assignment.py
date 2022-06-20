bd656344a5208302c9de3f58e00a53e1853f94c0
mugerw96 <mugerwaobadiah96@gmail.com>
2022-06-19 19:53:39 -0700

    init

--- 03_Assignment.py
+++ 03_Assignment.py
@@ -2,14 +2,20 @@
 
 def checkLIst(list):
     if len(list) == 0:
-        return 0
+        return "List empty"
+    elif len(list) == 4:
+        return "List Full"
     else:
         return 1
 
 
 # main code
-number = []
-if checkLIst(number):
-    print("List i empty")
+list = [2, 4, 5,4]
+if checkLIst(list):
+    print(checkLIst(list))
+    if(checkLIst(list)==1):
+        print("You can append")
+    else:
+        print("You cant append")
 else:
-    print("ITs ok")
+    print("ok")
